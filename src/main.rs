442da3a70dba37985953f991f06c2602cc4f429c
OGabrielPereira <ogabrielpereira@pm.me>
2022-03-06 14:35:26 -0300

    ideias

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+/*
+Ideias:
+
+No começo do programa será quando eu vou criar um "todo",
+vou poder colocar o nome dele e tudo mais,
+todos os to-do's serão colocados na mesma pasta,
+assim quando eu iniciar o programa eu vou precisar delecionar
+qual dos to-dos vou querer ver e etc.. 
+Conseguir escrever/apagar algo em um .txt.
+Mostrar o que está escrito nesse .txt
+Cada linha será uma task
+
+Pasta- todos:
+            lista1
+            lista2
+            lista3
+            ...
+
+lista1:
+    task1
+    task2
+    task3
+    ...
+
+Posso criar tasks, apagar, editar e marcar como comcluidas.
+*/
+
+
 fn main() {
     println!("Hello, world!");
 }
